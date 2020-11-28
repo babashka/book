25d58ba146fe6f3cc03323bd4b500151b066c49b
Michiel Borkent <michielborkent@gmail.com>
2020-11-28 15:42:38 +0100

    Usage: script

---
+++ src/usage/download_html.clj
@@ -0,0 +1,17 @@
+#!/usr/bin/env bb
+
+(require '[babashka.curl :as curl])
+
+(defn get-url [url]
+  (println "Downloading url:" url)
+  (curl/get url))
+
+(defn write-html [file html]
+  (println "Writing file:" file)
+  (spit file html))
+
+(let [[url file] *command-line-args*]
+  (when (or (empty? url) (empty? file))
+    (println "Usage: <url> <file>")
+    (System/exit 1))
+  (write-html file (get-url url)))
