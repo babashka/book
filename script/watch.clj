98c1d055956e8c505dce3104778d8a69e6d63a10
Michiel Borkent <michielborkent@gmail.com>
2021-02-10 12:55:36 +0100

    Update watch script

--- script/watch.clj
+++ script/watch.clj
@@ -1,19 +1,15 @@
 #!/usr/bin/env bb
 
 (require '[babashka.pods :as pods])
-(pods/load-pod "pod-babashka-filewatcher")
+
+(pods/load-pod 'org.babashka/filewatcher "0.0.1")
 (require '[pod.babashka.filewatcher :as fw])
 
-(pods/load-pod "pod-babashka-etaoin")
+(pods/load-pod 'org.babashka/etaoin "0.0.1")
 (require '[pod.babashka.etaoin :as eta])
 
-(require '[babashka.process :as p])
-
 (defn asciidoc []
-  (println "Compiling asciidoc")
-  (-> (p/process ["script/compile.clj"] {:inherit true})
-      p/check)
-  (println "Done"))
+  (load-file "script/compile.clj"))
 
 (asciidoc)
 
@@ -21,7 +17,10 @@
 (def cwd (System/getProperty "user.dir"))
 
 (require '[clojure.java.io :as io])
-(def index (io/file cwd "gh-pages/index.html"))
+(def index (io/file cwd (str "gh-pages/"
+                             (or (System/getenv "BABASHKA_BOOK_MAIN")
+                                 "master")
+                             ".html")))
 
 (eta/go browser (str (.toURL index)))
 
